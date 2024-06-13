9cc677d420e044e14c6aff62e243b55e12323e18
paddy <paddythepaddy@duck.com>
2024-06-13 09:38:54 +0800

    imp: Use clap's derive style

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,72 @@
 use anyhow::Context;
 use cal::Evaluator;
-use clap::{command, Arg, ArgAction, ArgGroup, ArgMatches};
+use clap::{Args, Parser};
 
-fn get_args() -> ArgMatches {
-    command!()
-        .version(git_version::git_version!(
-            prefix = "git:",
-            cargo_prefix = "cargo",
-            fallback = "wtf"
-        ))
-        .args([
-            Arg::new("expr").action(ArgAction::Append).required(true),
-            Arg::new("hex")
-                .long("hex")
-                .short('x')
-                .action(ArgAction::SetTrue)
-                .help(
-                    "Output with hexadecimal format \
-                (the result of the expression must be integer)",
-                ),
-            Arg::new("bits")
-                .long("bits")
-                .action(ArgAction::SetTrue)
-                .help(
-                    "Output list of 1 bits in the result \
-                (the result of the expression must be integer)",
-                ),
-            Arg::new("bin")
-                .long("bin")
-                .short('b')
-                .action(ArgAction::SetTrue)
-                .help(
-                    "Output with binary format \
-                (the result of the expression must be integer)",
-                ),
-            Arg::new("oct")
-                .long("oct")
-                .short('o')
-                .action(ArgAction::SetTrue)
-                .help(
-                    "Output with octal format \
-                (the result of the expression must be integer)",
-                ),
-            Arg::new("exp")
-                .long("exp")
-                .short('e')
-                .action(ArgAction::SetTrue)
-                .help("Print floating point number with scienfic notation"),
-        ])
-        .group(
-            ArgGroup::new("format")
-                .args(["oct", "bin", "hex", "bits", "exp"])
-                .multiple(false)
-                .required(false),
-        )
-        .get_matches()
+/// A cli calculator
+#[derive(Debug, Parser)]
+#[command(version=git_version::git_version!(
+    prefix = "git:",
+    cargo_prefix = "cargo",
+    fallback = "wtf"
+))]
+struct Cli {
+    #[command(flatten)]
+    format: PrintFormat,
+    #[arg(action = clap::ArgAction::Append)]
+    expr: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+#[group(required = false, multiple = false)]
+struct PrintFormat {
+    /// Output with hexadecimal format
+    /// (the result of the expression must be integer)
+    #[arg(long, short = 'x')]
+    hex: bool,
+    /// Output with octal format
+    /// (the result of the expression must be integer)
+    #[arg(long, short)]
+    oct: bool,
+    /// Output with binary format
+    /// (the result of the expression must be integer)
+    #[arg(long, short)]
+    bin: bool,
+    /// Print floating point number with scienfic notation
+    #[arg(long, short)]
+    exp: bool,
+    /// Output list of 1 bits in the result
+    /// (the result of the expression must be integer)
+    #[arg(long)]
+    bits: bool,
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = get_args();
-    let expr = args
-        .get_many::<String>("expr")
-        .context("Get argument failed")?
-        .map(String::as_str)
-        .collect::<Vec<_>>()
-        .join(" ");
+    let args = Cli::parse();
+    let expr = args.expr.join(" ");
     let evaluator = Evaluator::default();
     let result = evaluator.eval(&expr)?;
-    if args.get_flag("hex") {
+    if args.format.hex {
         println!(
             "{:X}",
             result.as_int().with_context(|| format!(
                 "--hex flag requires the result of the expression to be integer. Got {result:?}",
             ))?
         );
-    } else if args.get_flag("bin") {
+    } else if args.format.bin {
         println!(
             "{:b}",
             result.as_int().with_context(|| format!(
                 "--bin flag requires the result of the expression to be integer. Got {result:?}",
             ))?
         );
-    } else if args.get_flag("oct") {
+    } else if args.format.oct {
         println!(
             "{:o}",
             result.as_int().with_context(|| format!(
                 "--oct flag requires the result of the expression to be integer. Got {result:?}",
             ))?
         );
-    } else if args.get_flag("bits") {
+    } else if args.format.bits {
         let result = result.as_int().with_context(|| {
             format!(
                 "--bits flag requires the result of the expression to be integer. Got {result:?}"
@@ -101,7 +79,7 @@ fn main() -> anyhow::Result<()> {
             }
         }
         println!("{}", list.join(", "));
-    } else if args.get_flag("exp") {
+    } else if args.format.exp {
         let result = result.as_float().with_context(|| {
             format!(
                 "--exp flag requires the result of the expression to be floating point number. Got {result:?}"
